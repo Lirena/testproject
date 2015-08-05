ac22dac16a8c5b6b4f68a41abe14ca43c3e8d4c5
Lirena <irenalopina@gmail.com>
2015-08-05 21:21:27 +0300

    not finished
    I have not finished this asiignment, but I have a question

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['text']; 
+	$file = $_POST['file'];
+	echo $name."<br />".$file."<br />";
+$info = new SplFileInfo($file);
+var_dump($info->getExtension());
+    if ($info="jpg" ) {
+	  echo '1';
+	}
+}
 
-
-echo 'test!!';
-echo 'test!!';
+?>
